62068fb0afb474ab7e6729fce3dad9cbfd5879fd
Loïs <lois.puig@kctus.fr>
2016-10-19 10:35:59 +0200

    no message

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,20 +4,22 @@ class pagespeed::package inherits pagespeed {
 		cwd		=> "/tmp",
 		command => "curl -LO --silent 'https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb'",
 		path    => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
-		require => Package["curl"],
-	}
+		require => [Package["curl"], Exec['apt_update']],
+	} ->
 
 	exec { "pagespeed-install":
 		cwd		=> "/tmp",
 		path    => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
 		command => "dpkg -i mod-pagespeed-stable_current_amd64.deb; apt-get -f install",
-		require => [ Exec["pagespeed-download"], Class["apache"] ],
-	}
+		require => [Exec["pagespeed-download"], Class['apache']],
+	} ->
 
-	exec { "pagespeed-enable":
+	class { 'apache::mod::pagespeed': }
+	
+	/*exec { "pagespeed-enable":
 		command => "a2enmod pagespeed",
 		path    => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
 		require => [ Exec["pagespeed-install"], Class["apache"] ],
 		notify  => Class["apache::service"],
-	}
+	}*/
 }
